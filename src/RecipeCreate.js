0ea865f1e218b6a6989664e3da9c718c4f08f173
Sydney Workman <sworkman1996@gmail.com>
2022-03-22 18:25:44 -0500

    handleChange, handleSubmit, initialState

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,10 +1,32 @@
 import React, { useState } from "react";
 
-function RecipeCreate() {
+function RecipeCreate({ createRecipe }) {
 
   // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
   // TODO: Add the required input and textarea form elements.
   // TODO: Add the required submit and change handlers
+  const initialRecipe = {
+    name: '',
+    cuisine: '',
+    photo: '',
+    ingredients: '',
+    preparation: '',
+  };
+
+  const [recipe, setRecipe] = useState({ ...initialRecipe });
+  const handleChange = ({ target }) => {
+    setRecipe({
+      ...recipe,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createRecipe(recipe);
+
+    setRecipe({ ...initialRecipe });
+  };
   
   return (
     <form name="create">
