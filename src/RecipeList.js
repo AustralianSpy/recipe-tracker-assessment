77dd1bb64f1e00637558d0f0e008a2d5e6187b99
Sydney Workman <sworkman1996@gmail.com>
2022-03-22 20:14:22 -0500

    commenting

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -3,9 +3,8 @@ import Recipe from './Recipe';
 
 function RecipeList({ recipes, deleteRecipe }) {
   
-  // TODO: Display the list of recipes using the structure of table that is provided.
-  // TODO: Create at least one additional component that is used by this component.
-  // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
+  // Displays the list of recipes using the structure of table.
+  // Ternary operator to prevent error when trying to render an empty array of recipes.
 
   return (
     <div className="recipe-list">
